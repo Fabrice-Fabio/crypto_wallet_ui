3d2bc1cd139285097cb8663424c7f31a6565324e
fabiofabrice <fabricesumsa2000@gmail.com>
2024-04-27 20:16:36 +0200

    add amountCard

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 
 class Colors {
   static const pink = const Color(0xffD975BB);
-  static const darkBlue = const Color(0xff261863);
+  static const darkPurple = const Color(0xff261863);
+  static const lightPurple = const Color(0xff55389B);
   static const black = const Color(0xff171532);
   static const lightGrey = const Color(0xffEFF0F7);
   static const white = const Color(0xffffffff);
+  static const darkPink = const Color(0xffA03E82);
 }
--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,9 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       home: const MyHomePage(),
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: false,
+      ),
     );
   }
 }
@@ -37,7 +40,7 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Column(
+            const Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text("Hello Fabio", style: TextStyle(color: constants.Colors.pink, fontSize: 24)),
@@ -56,8 +59,72 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
         elevation: 0,
       ),
-      body: Center(
-        child: Text('Welcome to Flutter Guys Channel', style: TextStyle(fontSize: 20),),
+      body: Container(
+        height: 250,
+        margin: const EdgeInsets.all(5),
+        color: constants.Colors.white,
+        child: Stack(
+          children: [
+            Positioned(
+              top: 20,
+              bottom: 20,
+              right: 0,
+              left: 0,
+              child: Container(
+                margin: const EdgeInsets.all(15),
+                height: 150,
+                decoration:  const BoxDecoration(
+                  color: Colors.green,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.elliptical(250, 18),
+                    topRight: Radius.elliptical(250, 18),
+                    bottomRight: Radius.elliptical(250, 20),
+                    bottomLeft: Radius.elliptical(250, 20),
+                  ),
+                  gradient: LinearGradient(
+                      colors: [constants.Colors.darkPurple, constants.Colors.lightPurple]
+                  ),
+                ),
+                child: const Stack(
+                  children: [
+                    Positioned(
+                      top: 20,
+                      left: 20,
+                      child: Text('2.70% Today', style: TextStyle(
+                          color: constants.Colors.white,
+                          fontSize: 16
+                      )),
+                    ),
+                    Positioned(
+                      top: 40,
+                      left: 20,
+                      child: Text('\$12,670.90',
+                          style: TextStyle(
+                          color: constants.Colors.white,
+                          fontSize: 36,
+                          fontWeight: FontWeight.bold
+                      )),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 20,
+              right: 6,
+              child: Image.asset("assets/coin_stack.png"),
+            ),
+            Positioned(
+              bottom: 12,
+              left: 30,
+              child: FloatingActionButton(
+                onPressed: (){},
+                child: Icon(Icons.add),
+                backgroundColor: constants.Colors.darkPink,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
